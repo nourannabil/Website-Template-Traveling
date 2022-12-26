b93ad2e33f5af511dc76d0424d834c798b8e65e7
nouran <nourannabil12@gmail.com>
2022-12-27 02:05:48 +0300

    Add project files.

---
+++ Final Task1/js folder/first.js
@@ -0,0 +1,10 @@
+﻿
+const allImages = document.querySelectorAll(".card-destination img");
+
+const newImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI-%20lT6TcSB0vQ1r4MdN9NUcMtLT2jfKsnyzrnlAkCv-okumI80jMY9n-%20mNvEsGm4YF2Yk&usqp=CAU"
+
+allImages.forEach(img => {
+    img.addEventListener("click", (e1) => {
+        e1.target.src = newImage;
+    });
+})
